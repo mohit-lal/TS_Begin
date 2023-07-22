bd8c06061d05482e343073f35a1b551b92aaf109
mohit-lal <mohiit.lal@gmail.com>
2023-07-21 21:37:00 -0400

    Let const and arrow function

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,33 @@
-const user = "Mohit Lal";
+const userNames = "Max";
+//  Variable defined with const cannot be changed
+let age = 30;
+// let is dependent on the scope of the variables 
 
-console.log(user);
+age = 29;
+
+// var result;
+// function addn(a: number, b: number) {
+//     result = a+b;
+//     return result;
+// }
+// console.log("Hello world.");
+// console.log("Hello world.");
+// console.log(result);
+
+// Arrow function
+const addM = (a:number, b: number) => a+b; // for 1 expression no need for curly braces
+const addN = (a:number, b: number) => {
+    return a+b
+}
+
+console.log(addM(5,6));
+
+const pringOutput: (a: number | string) => void = output => console.log(output);
+
+const button = document.querySelector('button');
+
+if (button) {
+    // button.addEventListener('click', () => {}); // If you have no parameters you have to use and empty pair of parentheses: () => {}
+    button.addEventListener('click', event => console.log(event)); 
+}
+pringOutput(addN(5,3));
