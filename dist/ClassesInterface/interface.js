d7926a54f0755e0f0692f637d699d2b44a561193
mohit-lal <mohiit.lal@gmail.com>
2023-08-10 11:32:57 -0400

    classes and interface module done

--- dist/ClassesInterface/interface.js
+++ dist/ClassesInterface/interface.js
@@ -1,38 +1,27 @@
 "use strict";
-// // interface Person {
-// //   name: string;
-// //   age: number;
-// //   greet(phrase:string): void;
-// // }
-// // let user1: Person;
-// // user1 = {
-// //   name: "Max",
-// //   age: 30,
-// //   greet(phrase:string){
-// //     console.log(phrase + ' ' + this.name);
-// //   }
-// // };
-// // user1.greet("Hi there I am ");
-// interface Named {
-//     readonly name: string;
-//   }
-//   //Interface can be inherited
-//   interface Greetable extends Named{
-//     greet( phrase: string): void;
-//   }
-//   // while implementing interface we must adhere to interface structure
-//   // we should define properties and methods as that of interface
-//   class Person implements Greetable{ // implementing interface in a class
-//     name: string;
-//     age = 30;
-//     constructor (n: string) {
-//       this.name = n;
-//     }
-//     greet(phrase:string) {
-//       console.log(phrase + ' ' + this.name + ". My age is " + this.age);
-//     }
-//   }
-//   let user1: Greetable; // using interface as a type
-//   user1 = new Person("Max");
-//   user1.greet("Hi I am ");
-//   console.log(user1);
+// interface Person {
+//   name: string;
+//   age: number;
+// while implementing interface we must adhere to interface structure
+// we should define properties and methods as that of interface
+class Person {
+    constructor(n) {
+        this.outputName = '...';
+        this.age = 30;
+        if (n) {
+            this.name = n;
+        }
+    }
+    greet(phrase) {
+        if (this.name) {
+            console.log(phrase + ' ' + this.name + ". My age is " + this.age);
+        }
+        else {
+            console.log('Hi!');
+        }
+    }
+}
+let user1; // using interface as a type
+user1 = new Person();
+user1.greet("Hi I am ");
+console.log(user1);
